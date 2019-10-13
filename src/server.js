c4296346d9bb1a645cfde862741bddc07d5f870f
Nritya Kamath <nritya.k@gmail.com>
2019-10-13 16:17:31 -0700

    Inital view of homepage rendering

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const app = require("./app");
+const http = require("http");
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  }
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+server.on("listening", () => {
+    console.log(`server is listening for requests on port ${server.address().port}`);
+});
