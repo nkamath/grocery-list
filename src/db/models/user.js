0a1808e67d8111f4c2a37ecd7066d329f766f857
Nritya Kamath <nritya.k@gmail.com>
2019-10-13 18:25:51 -0700

    User model created; tests failing

---
+++ src/db/models/user.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: { msg: "must be a valid email" }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  
+  return User;
+};
